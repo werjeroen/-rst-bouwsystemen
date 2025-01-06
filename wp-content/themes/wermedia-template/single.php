7330592173e67b70ad029117848706521c38cd39
werjeroen <jeroen@wermedia.nl>
2025-01-06 10:45:16 +0100

    small improvements

--- wp-content/themes/wermedia-template/single.php
+++ wp-content/themes/wermedia-template/single.php
@@ -6,6 +6,6 @@
 
 get_header();
 
-get_template_part('template-parts/content/content', 'simple');
+get_template_part('template-parts/content/content', 'blocks');
 
 get_footer();
---
+++ wp-content/themes/wermedia-template/template-parts/content/content-subpages.php
@@ -0,0 +1,24 @@
+<?php
+    $hide_subpages = get_field('subpaginas_verbergen') ?? false;
+    $args = [
+        'post_type' => 'page',
+        'post_status' => 'publish',
+        'post_parent' => get_the_ID(),
+        'orderby' => 'menu_order',
+        'order' => 'ASC',
+        'posts_per_page' => 2,
+    ];
+    
+    $children = new WP_Query($args);
+?>
+<?php if ( !$hide_subpages && $children->have_posts()) : ?>
+    <section class="subpages content--even"> 
+        <div class="wrapper--default">
+            <div class="grid--two">
+                <?php while ($children->have_posts()) : $children->the_post(); ?>
+                    <?php get_template_part('template-parts/cards/card', 'link'); ?>
+                <?php endwhile; wp_reset_postdata(); ?>
+            </div>
+        </div>
+    </section>
+<?php endif; ?>
